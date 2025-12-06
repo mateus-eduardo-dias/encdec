02acca1ed0867a71b5cb1b918cdd2bb7f3653205
mateus-eduardo-dias <mateuseduqueiroz@proton.me>
2025-12-06 19:21:07 -0300

    feat: i/o formats parameters

--- index.js
+++ index.js
@@ -7,22 +7,25 @@ if (args.length == 0) {
 	console.log("Use --help")
 	process.exit(0)
 }
+
 const flags_list = ['d']
 const parameters_list = ['k', 'kf', 'iv', 'ivf', 't']
 const parameters = new Map()
 const flags = new Map()
+
+// Check flags and parameters
 for (let i = 0; i < args.length; i++) {
 	let arg = args[i]
-	if (arg.slice(0,2) == '--') {
+	if (arg.slice(0,2) == '--') { // Parameter
 		let parameter = arg.slice(2)
 		if (i != args.length - 1 && parameters_list.includes(parameter)) {
 			parameters.set(parameter, args[i+1])
 			i++;
 		} else {
-			console.error(`Error: Parameter ${arg} is invalid or has no value`)
+			console.error(`Error: Parameter ${arg} doesn't exist or has no value`)
 			process.exit(2)
 		}
-	} else if (arg.slice(0,1) == '-') {
+	} else if (arg.slice(0,1) == '-') { // Flag
 		let flag = arg.slice(1)
 		if (flags_list.includes(flag)) {
 			flags.set(flag, true)
@@ -33,30 +36,36 @@ for (let i = 0; i < args.length; i++) {
 	}
 }
 
-const text_formats = ['utf8', 'hex', 'base64']
+const text_formats = ['utf8', 'hex', 'base64', 'utf-8']
 
-const key_format = parameters.has('kf') ? parameters.get('kf') : 0
-if (key_format !== 0 && !text_formats.includes(key_format)) {
-	console.error(`Error: Parameter --kf has invalid value: ${key_format}`)
+const key_format = parameters.has('kf') ? parameters.get('kf') : 'hex'
+if (!text_formats.includes(key_format)) {
+	console.error(`Error: Parameter --kf is invalid: ${key_format}`)
 	process.exit(2)
 }
 
-const key =  parameters.has('k') ? Buffer.from(parameters.get('k'), key_format === 0 ? 'utf8' : key_format) : crypto.randomBytes(32)
+const key =  parameters.has('k') ? Buffer.from(parameters.get('k'), key_format) : crypto.randomBytes(32)
 if (key.length != 32) {
 	console.error(`Error: Key size is invalid: ${key.length}b (expected: 32b)`)
 	process.exit(2)
 }
 
 
-const iv_format = parameters.has('ivf') ? parameters.get('ivf') : 0
-if (iv_format !== 0 && !text_formats.includes(iv_format)) {
-	console.error(`Error: Parameter --ivf has invalid value: ${iv_format}`)
+const iv_format = parameters.has('ivf') ? parameters.get('ivf') : 'hex'
+if (!text_formats.includes(iv_format)) {
+	console.error(`Error: Parameter --ivf is invalid: ${iv_format}`)
 	process.exit(2)
 }
 
-const iv = parameters.has('iv') ? Buffer.from(parameters.get('iv'), iv_format === 0 ? 'utf8' : iv_format) : crypto.randomBytes(12)
+const iv = parameters.has('iv') ? Buffer.from(parameters.get('iv'), iv_format) : crypto.randomBytes(12)
 if (iv.length != 12) {
 	console.error(`Error: IV size is invalid: ${iv.length}b (expected: 12b)`)
+	process.exit(2)
+}
+
+const plaintext_format = parameters.has('tf') ? parameters.get('tf') : 'utf8'
+if (!text_formats.includes(plaintext_format)) {
+	console.error(`Error: Parameter --tf is invalid: ${plaintext_format}`)
 	process.exit(2)
 }
 
@@ -66,10 +75,16 @@ if (plaintext === 0) {
 	process.exit(2)
 }
 
-if (!flags.has('d')) {
+const output_format = parameter.has('of') ? parameters.get('of') : 'hex'
+if (!text_formats.includes(output_format)) {
+	console.error(`Error: Parameter --of is invalid: ${output_format}`)
+	process.exit(2)
+}
+
+if (!flags.has('d')) { // Encryption
 	const cipher = crypto.createCipheriv('aes-256-gcm', key, iv)
 
-	let encrypted = cipher.update(plaintext, 'utf8', 'hex')
+	let encrypted = cipher.update(plaintext, plaintext_format === 0 ? 'utf8' : plaintext_format, 'hex')
 	encrypted += cipher.final('hex')
 
 	const authTag = cipher.getAuthTag()
@@ -77,7 +92,7 @@ if (!flags.has('d')) {
 	console.log('AuthTag: ', authTag.toString('hex'))
 	console.log('IV: ', iv.toString('hex'))
 	console.log('Key: ', key.toString('hex'))
-} else {
+} else { // Decryption
 	let decipher = crypto.createDecipheriv('aes-256-gcm', key, iv)
 	decipher.setAuthTag(authTag)
 
